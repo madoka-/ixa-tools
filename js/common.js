0926184172eaa04716ad5d0eca6663902aef00b4
madoka <madoka666@gmail.com>
2011-04-02 12:59:02 +0900

    beta

--- js/common.js
+++ js/common.js
@@ -1,10 +1,12 @@
 // content scripts / common.js
 // 毎回読み込むスクリプトなので最小限にとどめる
 //
+// todo
+// scriptでやるよりcssでやる方がよさげ
 
 // onload
-$(document).ready(function(){
+/*$(document).ready(function(){
 	// コンテキストのHTMLやCSSを操作
-	// body{font-size:12px;margin:0;padding:0;line-height:1.5;font-family:"ＭＳ Ｐ明朝","細明朝体","ヒラギノ明朝 Pro W3";} textarea,input,select a p
+	// body{font-size:12px;margin:0;padding:0;line-height:1.5;font-family:"ＭＳ Ｐ明朝","細明朝体","ヒラギノ明朝 Pro W3";}
 	$('body, p, a, textarea, input, select').css('font-family', 'Meiryo, "メイリオ", "Hiragino Kaku Gothic Pro", "MS PGothic", "ＭＳ Ｐゴシック", "Osaka", helvetica, sans-serif');
-});
+});*/
